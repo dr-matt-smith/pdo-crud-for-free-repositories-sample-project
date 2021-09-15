5b9a5497685c4e84038cb7c6bf4aa5950d236e35
Matt Smith <dr_matt_smith@me.com>
2021-09-15 11:17:50 +0100

    index updated - to stop extra favicon requests

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
+<!-- prevent getting executed twice each time due to favicon requsts -->
+<link rel="icon" href="data:;base64,iVBORw0KGgo=">
+
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
