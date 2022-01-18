849ed2b591e4ba0b29bc82cab15f3f9a8ca488dc
dr-matt-smith <matt.smith@itb.ie>
2022-01-18 14:51:34 +0000

    removed legacy constant and comments from Movie class

--- src/Movie.php
+++ src/Movie.php
@@ -4,85 +4,48 @@ namespace Tudublin;
 
 class Movie
 {
-    const CREATE_TABLE_SQL99 =
-<<<HERE
-CREATE TABLE IF NOT EXISTS movie (
-    id integer PRIMARY KEY AUTO_INCREMENT,
-    title text,
-    price float,
-    category text
-)
-HERE;
-
     private int $id;
     private string $title;
     private float $price;
     private string $category;
 
-    /**
-     * @return int
-     */
     public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     */
     public function setId(int $id): void
     {
         $this->id = $id;
     }
 
-    /**
-     * @return string
-     */
     public function getTitle(): string
     {
         return $this->title;
     }
 
-    /**
-     * @param string $title
-     */
     public function setTitle(string $title): void
     {
         $this->title = $title;
     }
 
-    /**
-     * @return string
-     */
     public function getPrice(): string
     {
         return $this->price;
     }
 
-    /**
-     * @param string $price
-     */
     public function setPrice(string $price): void
     {
         $this->price = $price;
     }
 
-    /**
-     * @return string
-     */
     public function getCategory(): string
     {
         return $this->category;
     }
 
-    /**
-     * @param string $category
-     */
     public function setCategory(string $category): void
     {
         $this->category = $category;
     }
-
-
-
 }
