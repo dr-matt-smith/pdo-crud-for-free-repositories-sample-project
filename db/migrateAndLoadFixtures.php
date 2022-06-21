609a36d2d2fc196820c7e05f9aec1a675bd09622
Matt Smith <dr_matt_smith@me.com>
2022-06-22 00:33:01 +0100

    Update migrateAndLoadFixtures.php

--- db/migrateAndLoadFixtures.php
+++ db/migrateAndLoadFixtures.php
@@ -1,39 +1,15 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Tudublin\Movie;
 use Tudublin\MovieRepository;
 
 $movieRepository = new MovieRepository();
 
-// (1) drop then create table
-$movieRepository->dropTable();
-$movieRepository->createTable();
+$movieRepository->resetTable();
 
-// (2) delete any existing objects
-$movieRepository->deleteAll();
-
-// (3) create & insert objects
 $movieRepository->createAndInsert('Jaws', 9.99, 'horror');
 $movieRepository->createAndInsert('Jumanji', 7, 'entertainment');
 
-// (3) create objects
-// $m1 = new Movie();
-// $m1->setTitle('Jaws');
-// $m1->setPrice(9.99);
-// $m1->setCategory('horror');
-
-// $m2 = new Movie();
-// $m2->setTitle('Jumanji');
-// $m2->setPrice(9.99);
-// $m2->setCategory('entertainment');
-
-
-// (3) insert objects into DB
-//$movieRepository->insert($m1);
-//$movieRepository->insert($m2);
-
-// (4) test objects are there
 $movies = $movieRepository->findAll();
 print '<pre>';
 var_dump($movies);
