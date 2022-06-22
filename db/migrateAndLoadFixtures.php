acd14aeeb4995a2498dfa95ea1508d9ad0b9049f
Matt Smith <dr_matt_smith@me.com>
2022-06-22 10:20:23 +0100

    Update migrateAndLoadFixtures.php

--- db/migrateAndLoadFixtures.php
+++ db/migrateAndLoadFixtures.php
@@ -1,14 +1,23 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Tudublin\Movie;
 use Tudublin\MovieRepository;
 
 $movieRepository = new MovieRepository();
 
 $movieRepository->resetTable();
 
+// use handy method added to MovieRepository to create and insert object
 $movieRepository->createAndInsert('Jaws', 9.99, 'horror');
 $movieRepository->createAndInsert('Jumanji', 7, 'entertainment');
+
+// add new object without using createAndInsert(..,.)
+$jaws2 = new Movie();
+$jaws2->setTitle('Jaws2');
+$jaws2->setPrice(5.99);
+$jaws2->setCategory('horror');
+$movieRepository->insert($jaws2);
 
 $movies = $movieRepository->findAll();
 print '<pre>';
